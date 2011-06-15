52e8bc333a75b7ed3b8c33589d7fec665373e0d8
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-06-15 19:50:23 -0400

    Initial commit; fresh rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Living5to9Com::Application.config.secret_token = '519731dd431616f158ee44a0df234c36cd78fb779ffc71218b22b01a6b5b76f9e8796459825131a6acd1751c49f86032c58d07fab23d2819666a099f6af0bcee'
