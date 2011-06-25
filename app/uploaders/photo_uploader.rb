d0c8147d959b1539e4ae42409013c1fd7c177e33
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-06-25 16:34:33 -0400

    Generate reflected version
    Setup processors to generate reflected version of the large version. This would only be necessary for browsers that do not support CSS3 reflections, and unfortunately at the moment it looks like CarrierWave has a bug preventing the reflection from being recalled properly: https://github.com/jnicklas/carrierwave/issues/378

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,31 +2,14 @@
 
 class PhotoUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or ImageScience support:
   include CarrierWave::RMagick
-  # include CarrierWave::ImageScience
 
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/photos/#{model.id}"
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
 
   # Large format version, suitable for the standard photo viewer page
   version :large do
@@ -41,6 +24,16 @@ class PhotoUploader < CarrierWave::Uploader::Base
     # Reduce quality of version for web storage
     process :quality => [50]
 
+    version :reflected do
+
+      # process :reflection
+
+      # def filename
+      #   super.chomp(File.extname(super)) + '.png'
+      # end
+
+    end
+
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
@@ -49,14 +42,17 @@ class PhotoUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  private
 
   def quality(q=50)
     manipulate! {|img| img.write(current_path) {self.quality=q}}
+  end
+
+  def reflection
+    manipulate!(:format => "png") do |img|
+      img = img.wet_floor 0.4, 0.7
+      img.write(current_path) {self.quality=50}
+    end
   end
 
 end
