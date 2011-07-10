8739138720a516308e12dd871d60d6125951d999
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-07-10 13:13:36 -0400

    Configure capistrano to deploy "production" branch, rather than "master" or anything else

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, "living5to9.com"
 set :scm, :git
 set :repository,  "git@github.com:jcarlson/living5to9.com.git"
 set :deploy_via, :remote_cache
-set :branch, "deployment"
+set :branch, "production"
 
 # Deployment settings
 set :user, "jcarlson"
@@ -23,7 +23,13 @@ role :db,  "living5to9.com", :primary => true        # This is where Rails migra
 ssh_options[:forward_agent] = true
 set :rake, "bundle exec rake"
 set :default_environment, {
-  'PATH' => "~/.gems/bin:$PATH"
+  'HOME' => "/home/jcarlson",
+  'PATH' => "$HOME/local/bin:$HOME/.gems/bin:$PATH",
+  'GEM_PATH' => "$HOME/.gems:/usr/local/lib/ruby/gems/1.8",
+  'GEM_HOME' => "$HOME/.gems",
+  'LDFLAGS' => "-L/$HOME/local/lib",
+  'CPPFLAGS' => "-I/$HOME/local/include",
+  'LD_RUN_PATH' => "$HOME/local/lib"
 }
 
 # Additional tasks
