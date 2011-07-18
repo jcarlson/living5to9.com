8038ee16f87035d4651587338f9c7f6dbe2a46bf
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-07-17 21:35:04 -0400

    correcting an egregious mistake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,6 @@ Living5to9Com::Application.configure do
 end
 
 CarrierWave.configure do |config|
-  config.fog_directory  = 'living5to9.com'
+  config.fog_directory  = 'assets.living5to9.com'
   config.fog_host = 'http://assets.living5to9.com'
 end
