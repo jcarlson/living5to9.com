c7ae578afcf845b1dd742a4f47d0b8fea97be5e8
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-03 21:46:28 -0400

    Added HTTP Basic authentication to Active Admin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def authenticate!
+    authenticate_or_request_with_http_digest(Living5to9Com::Admin::Realm) do |username|
+      Living5to9Com::Admin::Users[username]
+    end
+  end
+
 end
