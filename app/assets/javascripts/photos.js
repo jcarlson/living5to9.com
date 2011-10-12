a3e5dd2d2f6570ca0ba6a748f99564d2022ca7cb
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-11 20:43:51 -0400

    Added shadowbox-js for übercool photo viewing

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,5 +1,9 @@
+//= require shadowbox
+
 $("#details").hide();
 $(".photo .image")
         .mouseenter(function() {$("#details").fadeIn(250);})
         .mouseleave(function() {$("#details").fadeOut(250);}
 );
+
+$(Shadowbox.init);
