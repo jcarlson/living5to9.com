a27f9e57e1058fc8a12a082bb9d9b23a567c00bf
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-12 01:09:40 -0400

    preserve cache directory between deploys

--- config/deploy/dragonfly.rb
+++ config/deploy/dragonfly.rb
@@ -3,7 +3,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :dragonfly do
     desc "Symlink the Dragonfly's Rack::Cache files"
     task :symlink, :roles => [:app] do
-      run "mkdir -p #{shared_path}/tmp/dragonfly && ln -nfs #{shared_path}/tmp/dragonfly #{release_path}/tmp/dragonfly"
+      run "mkdir -p #{shared_path}/tmp/cache && ln -nfs #{shared_path}/tmp/cache #{release_path}/tmp/cache"
     end
   end
 
