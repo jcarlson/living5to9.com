7dfd1d746a7ca02cb4cf539618bac3e73f92d5d0
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-12 18:31:47 -0400

    disable asset host for running locally as 'production'. also enable static asset serving for pure rails server.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,10 +29,10 @@ Living5to9Com::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "http://cdn%d.living5to9.com"
+  #config.action_controller.asset_host = "http://cdn%d.living5to9.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
