d3f6e51d0385563393a301089d5a3a03fdb22187
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-16 21:30:33 -0400

    Re-enable asset hosting... I think I've got the application working in a stable state.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Living5to9Com::Application.configure do
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # TODO: Re-enable asset_host once Dreamhost problems are worked out
-  #config.action_controller.asset_host = "http://cdn%d.living5to9.com"
+  config.action_controller.asset_host = "http://cdn%d.living5to9.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
