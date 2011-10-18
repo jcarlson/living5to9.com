16079e2c83422a14c8b679188240159da666f70e
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-18 19:51:33 -0400

    Customize the ActiveAdmin form interface for Post

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -4,14 +4,22 @@ ActiveAdmin.register Post do
   form do |f|
     f.inputs "Content" do
       f.input :title
-      f.input :content
+      f.input :content,
+        :hint => "Content is authored as Markdown"
     end
     f.inputs "Publication" do
-      f.input :publish_date, :hint => "Leave blank to publish now"
-      f.input :published
+      f.input :publish_date, 
+        :label => "Publication Date", 
+        :hint => "Leave blank to publish now"
+      f.input :published, 
+        :as => :radio, 
+        :label => "Publication State", 
+        :hint => "Set to 'Draft' to keep hidden from public view",
+        :collection => [["Published", true], ["Draft", false]]
     end
     f.inputs "Advanced" do
-      f.input :slug
+      f.input :slug, 
+        :hint => "Customize the permalink"
     end
     f.buttons
   end
