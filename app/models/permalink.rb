00966c7c6fed99dd733f5630734383288665aca3
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-28 16:39:00 -0400

    Validates that `permalink#slug` is not a reserved system route, i.e., it does does not collide with an existing route defined in `config/routes.rb`

--- app/models/permalink.rb
+++ app/models/permalink.rb
@@ -5,12 +5,19 @@ class Permalink < ActiveRecord::Base
   
   validates_presence_of   :slug
   validates_uniqueness_of :slug
+  validate :not_system_slug
   
 private
 
+  # Validates that the slug is not routable to a controller other than PermalinksController
+  def not_system_slug
+    route = Rails.application.routes.recognize_path "/#{slug}"
+    errors.add :slug, "is a reserved system route" unless route[:controller] == "permalinks"
+  end
+
   def validate_content
-    errors.add :content_id, "cannot be blank" unless content_id.present?
-    errors.add :content_type, "cannot be blank" unless content_type.present?
+    errors.add :content_id, "can't be blank" unless content_id.present?
+    errors.add :content_type, "can't be blank" unless content_type.present?
     errors.empty?
   end
   
