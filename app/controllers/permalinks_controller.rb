6ff5176a221ccfba8799c4482f1ea8d89d0447f8
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-29 16:34:32 -0400

    Decorates the @content before rendering it, if a decorator class is available. The original, undecorated object is still available as the @content attribute.

--- app/controllers/permalinks_controller.rb
+++ app/controllers/permalinks_controller.rb
@@ -10,7 +10,7 @@ class PermalinksController < ApplicationController
     params[:controller] = @content_type.pluralize.underscore
     
     # render the appropriate partial for this content, and do it in the layout
-    render @content, :layout => true
+    render :partial => partial, :object => decorate(@content), :layout => true
 
   end
 
@@ -18,14 +18,28 @@ protected
 
   def customize_view_path
     prepend_view_path "app/views/#{@content_type.pluralize.underscore}"
-    puts "VIEW PATH: #{view_paths}"
   end
 
+  def decorate(content)
+    decorated = begin
+      Module.const_get("#{@content_type}Decorator").decorate(content)
+    rescue NameError
+      content
+    end
+    decorated
+  end
+  
   def find_content
     # locate content by permalink, raising (404) error if none found
     permalink = Permalink.find_by_slug!(params[:slug])
     @content = permalink.content
     @content_type = permalink.content_type
+  end
+  
+  def partial
+    # We're going to have to make a guess about this...
+    # TODO: Is there some way to still defer to the partial renderer to determine the partial to use?
+    "#{@content_type.downcase.pluralize}/#{@content_type.downcase}"
   end
 
 end
