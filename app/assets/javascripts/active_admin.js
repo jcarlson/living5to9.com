eaa1728bd8e256a028b6c433a568cafeadcf0d39
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-29 19:20:34 -0400

    Fixed a typo

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,15 +1,16 @@
-// require active_admin/base // you suck for importing old jquery!
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
 
 /* Active Admin JS */
 
-$(function(){
+$(function() {
+  
   $(".datepicker").datepicker({dateFormat: 'yy-mm-dd'});
 
-  $(".clear_filters_btn").click(function(){
+  $(".clear_filters_btn").click(function() {
     window.location.search = "";
     return false;
   });
   
+});
