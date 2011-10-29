be2d2165f672ae408e4e46c5209629e8250b1ca8
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-29 12:45:36 -0400

    Customize view path based on @content being rendered
    * Moved content lookup into a before_filter
    * Set @content and @content_type for the view
    * Customized view_path in a before_filter, based on @content_type

--- app/controllers/permalinks_controller.rb
+++ app/controllers/permalinks_controller.rb
@@ -1,15 +1,31 @@
 class PermalinksController < ApplicationController
-  
+
+  before_filter :find_content
+  before_filter :customize_view_path
+
   # TODO: Need to figure out how to handle slugs with extensions, e.g., 'foo/bar/baz.jpg'
   def show
-    # locate content by permalink, raising error if none found
-    permalink = Permalink.find_by_slug!(params[:slug])
-    
+
     # set params[:controller] to model of content
-    params[:controller] = permalink.content_type.pluralize.underscore
+    params[:controller] = @content_type.pluralize.underscore
     
     # render the appropriate partial for this content, and do it in the layout
-    render permalink.content, :layout => true
+    render @content, :layout => true
+
+  end
+
+protected
+
+  def customize_view_path
+    prepend_view_path "app/views/#{@content_type.pluralize.underscore}"
+    puts "VIEW PATH: #{view_paths}"
+  end
+
+  def find_content
+    # locate content by permalink, raising (404) error if none found
+    permalink = Permalink.find_by_slug!(params[:slug])
+    @content = permalink.content
+    @content_type = permalink.content_type
   end
 
 end
