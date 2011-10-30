3df400cc496f4b108ad4db0449b61f11f6b33af3
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-10-29 20:59:29 -0400

    Yet another update to the deploy scripts

--- config/deploy/assets.rb
+++ config/deploy/assets.rb
@@ -12,6 +12,11 @@ Capistrano::Configuration.instance(:must_exist).load do
       system("bundle exec rake assets:precompile RAILS_GROUPS=assets RAILS_ENV=production")
     end
     
+    desc "Prepare shared assets directory"
+    task :symlink, :roles => [:app] do
+      run "mkdir -p #{shared_path}/assets && ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+    end
+    
     desc "Upload locally compiled assets"
     task :deploy, :roles => [:app] do
       upload("public/assets", "#{shared_path}", :recursive => true, :via => :scp)
@@ -22,5 +27,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   after 'deploy:update_code', 'assets:clean'
   after 'deploy:update_code', 'assets:precompile'
   after 'deploy:update_code', 'assets:deploy'
+  
+  before 'assets:deploy', 'assets:symlink'
 
 end
